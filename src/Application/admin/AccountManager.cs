3a820e00b2cf508baec5688d77caa1fa24f91dac
Albin Halitaj <albin.halitaj@gmail.com>
2021-11-19 23:50:41 +0100

    Login feature

---
+++ src/Application/admin/AccountManager.cs
@@ -0,0 +1,17 @@
+﻿using Data.admin;
+using Domain.Entities;
+
+namespace Application.admin
+{
+    public class AccountManager
+    {
+        private readonly AccountDal _accountManager;
+
+        public AccountManager(AccountDal accountManager)
+        {
+            _accountManager = accountManager;
+        }
+
+        public Employee Login(string username, string password) => _accountManager.Login(username, password);
+    }
+}
---
+++ src/Data/admin/Helpers/EncodeBase64.cs
@@ -0,0 +1,10 @@
+﻿namespace Data.admin.Helpers
+{
+    public class EncodeBase64
+    {
+        public static string Base64Encode(string plainText) {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+    }
+}
