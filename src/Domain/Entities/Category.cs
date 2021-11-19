0ba6c73b3871ec1204701b793e2fb30ed5c645c5
Albin Halitaj <albin.halitaj@gmail.com>
2021-11-19 15:13:20 +0100

    added category entity

---
+++ src/Domain/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public string Description { get; set; }
+        public bool Visibility { get; set; }
+    }
+}
