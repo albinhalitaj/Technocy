31a7dfa50669ee615525e0f86cf3fda4e87aa16a
Albin Halitaj <albin.halitaj@gmail.com>
2022-01-20 13:50:38 +0100

    added blog

---
+++ src/WebUI/Controllers/BlogController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace WebUI.Controllers
+{
+    public class BlogController : Controller
+    {
+           
+        [Route("/blog/smartfonet-me-te-mire-2022")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
